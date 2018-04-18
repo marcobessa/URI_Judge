e8d19cc6f8d63a292c76ac9dc3ad46f10470da40
Marco Bessa <marco.bessa@gmail.com>
2018-04-18 18:49:11 -0300

    resolver esse exercicio

---
+++ abracadabra.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <string.h>
+
+
+int main() {
+	
+	char  palavra[101];
+	int   i, tamanho;
+	fflush(stdin);
+	while (gets(palavra) != EOF ) {
+		
+		tamanho = strlen(palavra);
+		for (i = 0; i < tamanho; i++) {
+			
+			
+		}
+		
+		
+		
+	
+	
+	
+	
+	
+	}
+	// apenas para verificação
+	printf("\nA palavra lida é: %s", palavra);
+}
